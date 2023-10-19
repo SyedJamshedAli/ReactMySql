dd385fbe3101c60cbba0d8da0f3bf781d7a96f27
Jamshed <syed_jamshed_ali@yahoo.com>
2023-10-19 08:01:51 +0500

    19 oct morning

--- serverApp/api/account.js
+++ serverApp/api/account.js
@@ -1,14 +1,40 @@
+
 const express =require('express');
 const router=express.Router();
 const path=require('path');
 const data={};
-data.user=require('../data/user.json')
+data.user=require('../data/user.json');
+
 
 router.route('/')
 .get((req,res)=>{
-response.json(data.user)
+res.json(data.user)
+})
+.post((req,res)=>{
+    res.json({
+        "first_Name":req.body.firstname,
+        "last_name":req.body.lastname
+    })
+})
+.put((req,res)=>{
+    res.json({
+        "Put":req.body.firstname,
+        "Put_":req.body.lastname
+    })
+})
+.delete((req,res)=>{
+    res.json({
+        "delete":req.body.firstname,
+        "delete_":req.body.lastname
+    })
+})
+
+router.route('/:id')
+.get((req,res)=>{
+    res.json({
+        "urlParam":req.params.id        
+    })
 })
 
 
-
-module.exports=router;
+module.exports = router;
--- serverApp/server.js
+++ serverApp/server.js
@@ -1,59 +1,56 @@
-import  express  from "express"
-import mysql, { createConnection } from "mysql"
-import cors from "cors"
-import  jwt  from "jsonwebtoken"
-import bcrypt from "bcrypt"
-import cookieParser from "cookie-parser"
- 
+const express=require('express');
 const app= express();
-
+const path=require('path');
+const cors=require('cors');
 const port=process.env.Port||3500
+const jwt=require('jsonwebtoken');
+const bcrypt=require('bcrypt');
+const mysql=require('mysql');
+const createConnection=mysql.createConnection;
+const corsOptions=require('./config/corsOptions')
+
 app.use((req,res,next)=>{
     console.log(`${req.method} - ${req.path}`);
     next();
 })
 
-const whitelist=['http://localhost:3000'];
-const corsOptions={
-    origin:(origin,callback)=>{
-        if(whitelist.indexOf(origin)!== -1 || !origin){
-            callback(null,true);
-        }else
-        {
-            callback(new Error('not Allowed by cors'))
-        }
-    },
-    optionsSuccessStatus:200
-}
+
 app.use(cors(corsOptions));
 
 app.use(express.urlencoded({extended:false}));
 
 app.use(express.json())
 
-app.use(cookieParser)
+//app.use(cookieParser)
 
-app.use('/account','./api/account.js')
+//Server Static Files
+//app.use('/',express.static(path.join(__dirname,'/public')))
+
+//Routes
+app.use('/account',require('./api/account.js'));
 
 const db=createConnection({
-    host:'locahost',
-    user:'jam',
-    password:'123456',
+    host:'localhost',
+    user:'root',
+    password:'',
     database:'signup'
 })
 
-
+app.get('/',(req,res)=>{
+    res.send("done");
+})
 app.post('/signup',(req,res)=>{
     console.log('in api')
-    const sql="insert into login ('name','email','password') values (?)" 
+    const sql="insert into `signup`.`login` ('name','email','password') values (?)" 
     const values=[
         req.body.name,
         req.body.email,
         req.body.password
     ]
+    console.log(values.toString())
         db.query(sql,[values],(err,data)=>{
 
-            if(err) return res.json("Error");
+            if(err) return res.json(err.message);
             return res.json(data);
         })    
     
